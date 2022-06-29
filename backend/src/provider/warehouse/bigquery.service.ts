7ff5f13f3645e5a42a38cd6bd46e5d6cea28ce61
HM <hieumdd@gmail.com>
2022-06-29 17:57:30 +0700

    misc: Update fake data

--- backend/src/provider/warehouse/bigquery.service.ts
+++ backend/src/provider/warehouse/bigquery.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { BigQuery } from '@google-cloud/bigquery';
 import { faker } from '@faker-js/faker';
-import { range} from 'lodash'
+import { range } from 'lodash';
 
 import { CreateDto } from 'src/feature/plan/plan-item/plan-item.dto';
+
+const mockFloat = () => faker.datatype.number({
+    max: 0.99,
+    min: 0.01,
+    precision: 0.01,
+})
 
 const createMockPlanItem = (): CreateDto => ({
     sku: faker.random.numeric(13),
     startOfWeek: faker.date.soon(),
     region: 'north',
-    avgItemDiscount: faker.datatype.number({max: 0.99, precision: 0.01}),
-    avgOrderDiscount: faker.datatype.number({max: 0.99, precision: 0.01}),
-    discount: faker.datatype.number({max: 0.99, precision: 0.01}),
+    avgItemDiscount: mockFloat(),
+    avgOrderDiscount: mockFloat(),
+    discount: mockFloat(),
     workingDays: faker.datatype.number(),
     inventory: faker.datatype.number(),
     moq: faker.datatype.number(),
@@ -20,14 +26,14 @@ const createMockPlanItem = (): CreateDto => ({
 
 @Injectable()
 export class BigQueryService {
-    private bigQuery: BigQuery
-    
+    private bigQuery: BigQuery;
+
     constructor() {
-        this.bigQuery = new BigQuery()
+        this.bigQuery = new BigQuery();
     }
 
     async query() {
-        return range(1, 400).map(() => createMockPlanItem())
+        return range(1, 400).map(() => createMockPlanItem());
         // return this.bigQuery.query(query).then(([rows]) => rows);
     }
 }
