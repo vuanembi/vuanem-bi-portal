8969e2ae14b5d29423f5c5db831849e9d880c87b
HM <hieumdd@gmail.com>
2022-07-01 18:25:19 +0700

    feat: Add Table

--- frontend/pages/demand-planning/[id].tsx
+++ frontend/pages/demand-planning/[id].tsx
@@ -5,7 +5,7 @@ import { VStack } from '@chakra-ui/react';
 import { apiClient } from '../../feature/demand-planning/lib';
 
 import Header from '../../feature/demand-planning/component/PlanDetails/Header/Header';
-import Table from '../../feature/demand-planning/component/PlanDetails/Table/Table';
+import ItemTable from '../../feature/demand-planning/component/PlanDetails/Table/Table';
 
 import { Plan as PlanPageProps } from '../../feature/demand-planning/type';
 
@@ -13,7 +13,7 @@ const Plan: NextPage<{ plan: PlanPageProps }> = ({ plan }) => {
     return (
         <VStack alignItems="stretch">
             <Header {...plan} />
-            <Table {...plan}/>
+            <ItemTable {...plan} />
         </VStack>
     );
 };
