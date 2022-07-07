6a9e82f8b224605fc1f450b4be0443b0a809b686
HM <hieumdd@gmail.com>
2022-07-07 14:57:26 +0700

    feat: Remove findAll & findOne Plan Item

--- backend/src/feature/demand-planning/plan-item/plan-item.controller.ts
+++ backend/src/feature/demand-planning/plan-item/plan-item.controller.ts
@@ -1,23 +1,13 @@
-import { Controller, Get, Put, Param, Query, Body } from '@nestjs/common';
+import { Controller, Put, Param, Body } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { PlanItemService } from './plan-item.service';
-import { GetPlanItemsDto, UpdatePlanItemDto } from './plan-item.dto';
+import { UpdatePlanItemDto } from './plan-item.dto';
 
 @ApiTags('Demand Planning / Plan Item')
 @Controller('plan-item')
 export class PlanItemController {
     constructor(private readonly planItemService: PlanItemService) {}
-
-    @Get()
-    findAll(@Query() query: GetPlanItemsDto) {
-        return this.planItemService.findAll(query);
-    }
-
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.planItemService.findOne(+id);
-    }
 
     @Put(':id')
     update(@Param('id') id: string, @Body() body: UpdatePlanItemDto) {
--- backend/src/feature/demand-planning/plan-item/plan-item.service.ts
+++ backend/src/feature/demand-planning/plan-item/plan-item.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { PlanItem } from './plan-item.entity';
-import { GetPlanItemsDto, UpdatePlanItemDto } from './plan-item.dto';
+import { UpdatePlanItemDto } from './plan-item.dto';
 
 @Injectable()
 export class PlanItemService {
@@ -11,16 +11,6 @@ export class PlanItemService {
         @InjectRepository(PlanItem)
         private readonly planItemRepository: Repository<PlanItem>,
     ) {}
-
-    findAll({ planId }: GetPlanItemsDto) {
-        return this.planItemRepository.find({
-            where: { plan: { id: planId } },
-        });
-    }
-
-    findOne(id: number) {
-        return this.planItemRepository.findOneBy({ id });
-    }
 
     async update(id: number, updatePlanItemDto: UpdatePlanItemDto) {
         return this.planItemRepository.update(id, updatePlanItemDto);
