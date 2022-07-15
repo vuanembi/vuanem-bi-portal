ccd9a3353e0e233a74c48f6d2c9778c90cc827dd
HM <hieumdd@gmail.com>
2022-07-15 17:00:14 +0700

    fix: Fix docker networks

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const getApiClient = (module: string) => {
-    const baseURL = [process.env.NEXT_PUBLIC_API_URL || '/api', module].join(
-        '/',
-    );
+    const baseURL = [
+        process.env.API_URL || process.env.NEXT_PUBLIC_API_URL,
+        module,
+    ].join('/');
     return axios.create({ baseURL });
 };
 
